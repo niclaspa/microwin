2f7018979182d103c77282d9ce5067c8e0ffe5d1
Niclas Paulsson <niclas.paulsson@hotmail.com>
2015-05-12 12:10:06 +0100

    add FuncEqualityComparer class

---
+++ src/Microwin/FuncEqualityComparer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microwin
+{
+    public class FuncEqualityComparer<T> : IEqualityComparer<T>
+    {
+        private Func<T, T, bool> equals;
+        private Func<T, int> getHashCode;
+
+        public FuncEqualityComparer(Func<T, T, bool> equals, Func<T, int> getHashCode)
+        {
+            this.equals = equals;
+            this.getHashCode = getHashCode;
+        }
+
+        public bool Equals(T x, T y)
+        {
+            return equals(x, y);
+        }
+
+        public int GetHashCode(T obj)
+        {
+            return getHashCode(obj);
+        }
+    }
+}
