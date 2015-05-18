74e6c78bbad1b6351c0820f10e2e052b358a50e5
Niclas Paulsson <niclas.paulsson@hotmail.com>
2015-05-18 15:01:46 +0100

    add some string utils

--- src/Microwin/Extensions/StringExtensions.cs
+++ src/Microwin/Extensions/StringExtensions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Microwin.Extensions
@@ -18,6 +19,54 @@ namespace Microwin.Extensions
         public static string InvariantFormat(this string value, params object[] args)
         {
             return string.Format(CultureInfo.InvariantCulture, value, args);
+        }
+
+        /// <summary>
+        /// Removes the diacritics.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        /// <returns></returns>
+        public static string RemoveDiacritics(this string text)
+        {
+            string formD = text.Normalize(NormalizationForm.FormD);
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char ch in formD)
+            {
+                UnicodeCategory uc = CharUnicodeInfo.GetUnicodeCategory(ch);
+                if (uc != UnicodeCategory.NonSpacingMark)
+                {
+                    sb.Append(ch);
+                }
+            }
+
+            return sb.ToString().Normalize(NormalizationForm.FormC);
+        }
+
+        public static string RemoveAllWhitespace(this string value)
+        {
+            return Regex.Replace(value, @"\s*", string.Empty);
+        }
+
+        /// <summary>
+        /// UnEscapes unicode characters in a string.
+        /// </summary>
+        /// <param name="s">The string.</param>
+        /// <returns></returns>
+        public static string UnEscapeUnicode(this string s)
+        {
+            if (!String.IsNullOrEmpty(s))
+            {
+                string esc = Regex.Replace(s, @"\\u([\dA-Fa-f]{4})", v => ((char)Convert.ToInt32(v.Groups[1].Value, 16)).ToString());
+                return esc;
+            }
+
+            return s;
+        }
+
+        public static string ToLettersOnly(this string s)
+        {
+            return new string(s.ToLowerInvariant().Where(c => Char.IsLetter(c)).ToArray());
         }
     }
 }
